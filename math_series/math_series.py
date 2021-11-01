1c5640981f8d14dd94410c517496e98cf7489b48
issasalman <issasalmanhu1996@gmail.com>
2021-11-01 21:59:46 +0200

    done

--- math_series/math_series.py
+++ math_series/math_series.py
@@ -5,12 +5,19 @@
 
 
 def fibonacci(n):
+     """ 
+     The function was created to take number that its length of index given and it will start from 0 and 1
+     """
      if n <= 1:
        return n
      else:
        return(fibonacci(n-1) + fibonacci(n-2))
 
 def lucas(n):
+    """ 
+    The function was created to take number that its length of index  given and it will start from 2 and 1
+    """
+
     if n == 0:
         return 2
     if n == 1:
@@ -21,6 +28,9 @@ def lucas(n):
 
 
 def sum_series(n,firstNum=0,secondNum=1): 
+    """
+    The function was created to take number that its length of index  given and it will start from first and second
+    """
     if n<0:
         return "Cant do sum series on negative num"
     if n==0:
@@ -30,7 +40,7 @@ def sum_series(n,firstNum=0,secondNum=1):
     return sum_series(n-1, firstNum, secondNum) + sum_series(n-2, firstNum, secondNum)       
 
 
-# print(sum_series(2,5,5))
+print(sum_series(5))
 # print(lucas(5))
 # print(fibonacci(5))
 
